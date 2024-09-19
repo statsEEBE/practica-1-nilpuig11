a6dfca8d178a323fccb429f07cfd7cee282e26d9
nil.puig.biosca <nil.puig.biosca@auleseebe.upc.edu>
2024-09-19 11:52:58 +0200

    ...

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,8 @@
 #Codigo para problema 3
+x<-c(-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,-5,5)
+sum(x)
+y<-x[-c(21,2,26)]#elimina les posicions
+y
+sum(exp(x))-sum(exp(y))
+#y[19] per mirar la posicio 19
+#y24] per mirar la posicio 24
