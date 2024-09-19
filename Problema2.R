52b880e6a525ef07126d53762995bf64854fddd3
nil.puig.biosca <nil.puig.biosca@auleseebe.upc.edu>
2024-09-19 11:51:57 +0200

    ...

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,11 @@
 #Codigo para problema 2
+
+ls() #listar todas las variables hasta el momento
+rm(list=ls())#borrem les dades que haviem guardat abans
+mis_dades <- mtcars
+mis_dades
+data=mtcars
+data
+hist(data$mpg) #aquesta linia seria ja la resposta al problema 2
+pie(data$mpg)
+pie(table(data$mpg))
